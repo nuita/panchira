b484f507a54263150e804f4d5bd8525a5aed3e71
kyp <kyp@kmc.gr.jp>
2020-05-13 02:19:32 +0900

    add test for DlsiteResolver

--- test/panchira_test.rb
+++ test/panchira_test.rb
@@ -33,4 +33,14 @@ class PanchiraTest < Minitest::Test
     attributes = Panchira.fetch(url)
     assert_equal 'https://example.com/', attributes[:canonical_url]
   end
+
+  def test_fetch_dlsite_correctly
+    url = 'https://www.dlsite.com/maniax-touch/dlaf/=/t/p/link/work/aid/miuuu/id/RJ255695.html'
+    attributes = Panchira.fetch(url)
+
+    assert_match '性欲処理される生活。', attributes[:title]
+    assert_match '事務的な双子メイドが、両耳から囁きながら、ご主人様のおちんぽのお世話をしてくれます♪', attributes[:description]
+    assert_equal 'https://img.dlsite.jp/modpub/images2/work/doujin/RJ256000/RJ255695_img_main.jpg', attributes[:image][:url]
+  end
+
 end
