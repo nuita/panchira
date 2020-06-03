b1e68fb7139af27c43021e611243b400c40f4a3b
ahis1503 <ahis1503@gmail.com>
2020-06-03 13:37:38 +0000

    Facebookになりすます代わりにPanchiraBotを使う

--- lib/panchira/resolvers/twitter_resolver.rb
+++ lib/panchira/resolvers/twitter_resolver.rb
@@ -3,7 +3,7 @@
 module Panchira
   class TwitterResolver < Resolver
     URL_REGEXP = %r{twitter.com}.freeze
-    USER_AGENT = "facebookexternalhit/1.1 (compatible; Panchira/#{VERSION}; +https://github.com/nuita/panchira)"
+    USER_AGENT = "Mozilla/5.0 (compatible; PanchiraBot/#{VERSION}; +https://github.com/nuita/panchira)"
   end
 
   ::Panchira::Extensions.register(Panchira::TwitterResolver)
