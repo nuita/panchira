baeea3d112838e69e97dd8541fb5c3f489d7cec1
Kugayama Nana <nonamea774@gmail.com>
2020-06-18 20:38:15 +0900

    impl novel18 tag get

--- lib/panchira/resolvers/narou_resolver.rb
+++ lib/panchira/resolvers/narou_resolver.rb
@@ -5,6 +5,7 @@ require 'net/https'
 module Panchira
   class NarouResolver < Resolver
     URL_REGEXP = %r{novel18\.syosetu\.com/}.freeze
+    ID_REGEXP = %{novel18\.syosetu\.com/(?<id>[^/]+)}.freeze
 
     def fetch_page(uri)
       u = URI.parse(uri)
@@ -13,6 +14,14 @@ module Panchira
       res = http.get u.request_uri, { 'cookie' => 'over18=yes;' }
 
       Nokogiri::HTML.parse(res.body, uri)
+    end
+
+    def parse_tags
+      id = @url.match(ID_REGEXP)[:id]
+      return [] unless id
+
+      desc = fetch_page("https://novel18.syosetu.com/novelview/infotop/ncode/#{id}/")
+      desc.xpath('//*[@id="noveltable1"]/tr[3]')&.text&.split("\n\n\n")&.dig(1)&.split(' ') # つらい。
     end
   end
 
