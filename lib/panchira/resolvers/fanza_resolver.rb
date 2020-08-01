9187e7693e54798d52f10a4934e718d9dc1c9fe1
kyp <kyp@kmc.gr.jp>
2020-08-01 18:35:49 +0900

    対象を電子書籍に限定

--- lib/panchira/resolvers/fanza_resolver.rb
+++ lib/panchira/resolvers/fanza_resolver.rb
@@ -3,10 +3,16 @@
 require 'net/https'
 
 module Panchira
-  class FanzaResolver < Resolver
-    URL_REGEXP = /dmm\.co\.jp/.freeze
-    COOKIE = 'age_check_done=1;'
+  module Fanza
+    class FanzaBookResolver < Resolver
+      URL_REGEXP = %r{book\.dmm\.co\.jp\/}.freeze
+      COOKIE = 'age_check_done=1;'
+
+      def parse_image
+        @page.css('.m-imgDetailProductPack/@src').first.to_s
+      end
+    end
   end
 
-  ::Panchira::Extensions.register(Panchira::FanzaResolver)
+  ::Panchira::Extensions.register(Panchira::Fanza::FanzaBookResolver)
 end
--- test/resolvers/fanza_test.rb
+++ test/resolvers/fanza_test.rb
@@ -3,10 +3,11 @@
 require 'test_helper'
 
 class FanzaTest < Minitest::Test
-  def test_fetch_fanza_book
+  def test_fetch_fanza_books
     url = 'https://book.dmm.co.jp/detail/b425aakkg00140/'
     result = Panchira.fetch(url)
 
     assert_match '10から始める英才教育', result.title
+    assert_equal 'https://ebook-assets.dmm.co.jp/digital/e-book/b425aakkg00140/b425aakkg00140pl.jpg', result.image
   end
 end
