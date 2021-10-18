cc3681567cae246237ac641ced5d381e1f439075
kyp <kyp@kmc.gr.jp>
2021-10-18 22:36:37 +0900

    テストを名前の変化に強そうなツイートに変更

--- test/resolvers/twitter_test.rb
+++ test/resolvers/twitter_test.rb
@@ -4,11 +4,11 @@ require 'test_helper'
 
 class TwitterTest < Minitest::Test
   def test_fetch_twitter
-    url = 'https://twitter.com/mirakuru_rodeo/status/1266016566769926144'
+    url = 'https://twitter.com/bot_erection/status/1412029253906886657'
     result = Panchira.fetch(url)
 
-    assert_match 'みらくるる', result.title
-    assert_match '2巻発売中', result.description
+    assert_match '勃起タイムbot', result.title
+    assert_match '君は中学生なのになかなかの勃起サイズをしているね？', result.description
     assert_match 'https://pbs.twimg.com/media', result.image.url
   end
 end
